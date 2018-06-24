30d62ec4fdc4f5c7ebc7c88be941a8f352b7eae0
Wiktor Obrebski <wiktor.obrebski@10clouds.com>
2018-06-24 17:30:22 +0200

    Proper pass for tests database instance [api]

--- alcarin_api/apps/alcarin/config/test.exs
+++ alcarin_api/apps/alcarin/config/test.exs
@@ -3,8 +3,8 @@ use Mix.Config
 # Configure your database
 config :alcarin, Alcarin.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "alcarin",
+  password: "alcarin",
   database: "alcarin_test",
-  hostname: "localhost",
+  hostname: "postgres.link",
   pool: Ecto.Adapters.SQL.Sandbox
